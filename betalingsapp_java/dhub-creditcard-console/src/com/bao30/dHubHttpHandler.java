7c8abfcc280aa730834c93e5255c761d776bb903
Joakim Vrangum <joakimvrangum@gmail.com>
2019-05-15 09:56:25 +0200

    Endret addresse fra lokal til live-server

--- betalingsapp_java/dhub-creditcard-console/src/com/bao30/dHubHttpHandler.java
+++ betalingsapp_java/dhub-creditcard-console/src/com/bao30/dHubHttpHandler.java
@@ -9,7 +9,7 @@ import java.net.URL;
 
 public class dHubHttpHandler {
 
-    public static final String DHUB_SERVER = "http://127.0.0.1:8000/api/transaction/test";
+    public static final String DHUB_SERVER = "http://www.vrangum.com/dhub/api/transaction/test";
 
     public static void dHubTransactionPOST(String card_nr, Double pris, String varetekst) throws IOException {
         URL obj = new URL(DHUB_SERVER);
